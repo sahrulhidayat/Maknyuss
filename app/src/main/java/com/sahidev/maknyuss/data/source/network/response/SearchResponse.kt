5da6e1ea5f8579e7bdadab735f57d1f6be6b9234
Sahrul Hidayat <sahrulhint@gmail.com>
2024-01-25 14:27:08 +0700

    Simplify the API responses

--- app/src/main/java/com/sahidev/maknyuss/data/source/network/response/SearchResponse.kt
+++ app/src/main/java/com/sahidev/maknyuss/data/source/network/response/SearchResponse.kt
@@ -7,11 +7,11 @@ data class SearchResponse(
 	@field:SerializedName("number")
 	val number: Int,
 
-	@field:SerializedName("totalResults")
-	val totalResults: Int,
-
 	@field:SerializedName("offset")
 	val offset: Int,
+
+	@field:SerializedName("totalResults")
+	val totalResults: Int,
 
 	@field:SerializedName("results")
 	val results: List<ResultsItem>
@@ -19,36 +19,12 @@ data class SearchResponse(
 
 data class ResultsItem(
 
-	@field:SerializedName("image")
-	val image: String,
-
-	@field:SerializedName("nutrition")
-	val nutrition: Nutrition,
-
 	@field:SerializedName("id")
 	val id: Int,
 
 	@field:SerializedName("title")
 	val title: String,
 
-	@field:SerializedName("imageType")
-	val imageType: String
-)
-
-data class Nutrition(
-
-	@field:SerializedName("nutrients")
-	val nutrients: List<NutrientsItem>
-)
-
-data class NutrientsItem(
-
-	@field:SerializedName("amount")
-	val amount: Any,
-
-	@field:SerializedName("unit")
-	val unit: String,
-
-	@field:SerializedName("name")
-	val name: String
+	@field:SerializedName("image")
+	val image: String,
 )
