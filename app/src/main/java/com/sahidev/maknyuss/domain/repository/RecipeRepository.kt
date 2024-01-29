2fadbd70d5ba55b886a111c1c80ef229b7b4296b
Sahrul Hidayat <sahrulhint@gmail.com>
2024-01-29 11:00:51 +0700

    Updating repository implementation offline first approach

--- app/src/main/java/com/sahidev/maknyuss/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/sahidev/maknyuss/domain/repository/RecipeRepository.kt
@@ -1,5 +1,6 @@
 package com.sahidev.maknyuss.domain.repository
 
+import com.sahidev.maknyuss.domain.Resource
 import com.sahidev.maknyuss.domain.model.Equipment
 import com.sahidev.maknyuss.domain.model.Ingredient
 import com.sahidev.maknyuss.domain.model.Instruction
@@ -7,10 +8,10 @@ import com.sahidev.maknyuss.domain.model.Recipe
 import kotlinx.coroutines.flow.Flow
 
 interface RecipeRepository {
-    fun searchRecipe(query: String): Flow<List<Recipe>>
-    suspend fun getRecipeInfo(id: Int): Recipe
-    suspend fun getRandomRecipe(): Recipe
-    fun getSavedRecipes(): Flow<List<Recipe>>
+    fun searchRecipe(query: String, offset: Int): Flow<Resource<List<Recipe>>>
+    suspend fun getRecipeInfo(id: Int): Flow<Resource<Recipe>>
+    suspend fun getRandomRecipe(): Flow<Resource<List<Recipe>>>
+    fun getSavedRecipes(): Flow<Resource<List<Recipe>>>
     suspend fun addRecipe(recipe: Recipe)
     suspend fun deleteRecipe(id: Int)
     suspend fun addInstruction(instruction: Instruction)
